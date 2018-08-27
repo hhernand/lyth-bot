c4136ac87beb9fc3db42a7d613dd896169525a16
Hazel Hernandez <halh2012@gmail.com>
2018-08-26 17:47:51 -0700

    added role assign command

---
+++ commands/other/roles.js
@@ -0,0 +1,20 @@
+module.exports = {
+  assign: function(msg){
+    let type = msg.content.split(' ')[1];
+    if (type == 'customs') {
+      msg.member.addRole('483425658077446163');
+      msg.channel.send('You now have the customs role.');
+    }
+    else if (type == 'seeking') {
+      msg.member.addRole('483426748822978570');
+      msg.channel.send('You now have the seeking role.');
+    }
+    else if (type == 'gift') {
+      msg.member.addRole('483427545404932119');
+      msg.channel.send('You now have the gift role.');
+    }
+    else {
+      msg.channel.send('That role doesn\'t exist or you can\'t give yourself that role.');
+    }
+  }
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bot = new Discord.Client();
 const mysql = require('mysql');
 
 const db = require('require-dir-all')('./commands/db');
+const other = require('require-dir-all')('./commands/other');
 
 var con;
 if(process.env.JAWSDB_URL) {
@@ -11,6 +12,12 @@ if(process.env.JAWSDB_URL) {
 
 bot.on('message', (message) => {
   let msg = message.content.toLowerCase();
+
+  if (message.channel.id == '466370937562857474') {
+    if (msg.startsWith('!assign ')){
+      other.roles.assign(message);
+    }
+  }
 
   if (msg == '!register') {
     db.player.register(message, con);
