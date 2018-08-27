f82ea8371949e6470bb3bc77a23d0483c58b87a4
Hazel Hernandez <halh2012@gmail.com>
2018-08-26 18:06:42 -0700

    tags sender on role assign

--- commands/other/roles.js
+++ commands/other/roles.js
@@ -3,18 +3,18 @@ module.exports = {
     let type = msg.content.split(' ')[1];
     if (type == 'customs') {
       msg.member.addRole('483425658077446163');
-      msg.channel.send('You now have the customs role.');
+      msg.channel.send(msg.author + ' You now have the customs role.');
     }
     else if (type == 'seeking') {
       msg.member.addRole('483426748822978570');
-      msg.channel.send('You now have the seeking role.');
+      msg.channel.send(msg.author + ' You now have the seeking role.');
     }
     else if (type == 'gift') {
       msg.member.addRole('483427545404932119');
-      msg.channel.send('You now have the gift role.');
+      msg.channel.send(msg.author + ' You now have the gift role.');
     }
     else {
-      msg.channel.send('That role doesn\'t exist or you can\'t give yourself that role.');
+      msg.channel.send(msg.author + ' That role doesn\'t exist or you can\'t give yourself that role.');
     }
   }
 }
